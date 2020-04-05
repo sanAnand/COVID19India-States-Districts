7cd90f9fdc2d8aa0c7316860ddb2466187689dc1
JubbaOnJeans <anand.sandesh@gmail.com>
2020-04-05 23:35:29 +0530

    Delete DistrictState.py

--- DistrictState.py
+++
@@ -1,8 +0,0 @@
-import json 
-from external import extract_element_from_json, unique
-from collections import Counter
-# some JSON:
-with open('raw_data.json') as json_file:
-    y = json.load(json_file)
-DS = extract_element_from_json(y,["raw_data","detectedstate"])
-print(Counter(DS))
